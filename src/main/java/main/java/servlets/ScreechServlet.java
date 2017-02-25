a4b55b69f8fd8806fd5b8be93b749541aaaf91c5
BrianCoveney <coveneybrian@gmail.com>
2017-02-25 17:48:53 +0000

    speed calculation completed

--- src/main/java/main/java/servlets/ScreechServlet.java
+++ src/main/java/main/java/servlets/ScreechServlet.java
@@ -34,6 +34,13 @@ public class ScreechServlet extends HttpServlet {
     private static final int EQUATION_CONST = 30;
     // array of road surface types
     private String surfaceTypes[] = { "Cement", "Asphalt", "Gravel", "Ice", "Snow" };
+    private String carName;
+    private double skidLen1, skidLen2, skidLen3, skidLen4;
+    private int numOfSkidMarks;
+    private double averageSkidLength = 0;
+    private String surfaceType;
+    private double breakingEfficiency;
+    private double dragFactor;
 
 
     public ScreechServlet() {
@@ -41,34 +48,35 @@ public class ScreechServlet extends HttpServlet {
     }
 
 
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    }
+
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        double skidLength = Double.parseDouble(request.getParameter("skidlength"));
-        int skidMark = Integer.parseInt(request.getParameter("skidmark"));
-        double averageSkidLength = 0.0;
-        double dragFactor = 0.75;
-        double breakingEfficiency = 1.0;
 
+        // set vars equal to form parameters
+        carName = request.getParameter("skidlength");
+        numOfSkidMarks = Integer.parseInt(request.getParameter("skidmark"));
+        skidLen1 = Double.parseDouble(request.getParameter("skidmarklength1"));
+        skidLen2 = Double.parseDouble(request.getParameter("skidmarklength2"));
+        skidLen3 = Double.parseDouble(request.getParameter("skidmarklength3"));
+        skidLen4 = Double.parseDouble(request.getParameter("skidmarklength4"));
+        surfaceType = request.getParameter("surface");
 
-//        String choice = request.getParameter("surface");
+        // set variable equal to the return from the method No. 1 calculateAverageSkidDistance()
+        double avgSkidLength = calculateAverageSkidDistance(numOfSkidMarks, skidLen1, skidLen2, skidLen3, skidLen4);
 
+        // drag factor
+        double dragFactor = setDragFactor(surfaceType);
 
-        if(skidMark == 4) {
-            averageSkidLength = (skidLength * 4) / 4;
-        } else if(skidMark == 3) {
-            averageSkidLength = (skidLength * 3) / 3;
-        } else if(skidMark == 2) {
-            averageSkidLength = (skidLength * 2) / 2;
-        } else if(skidMark == 1) {
-            averageSkidLength = skidLength;
-        }
+        // breaking efficiency
+        double breakEff = calculateBreakingEfficiency(numOfSkidMarks);
 
-        // get result from the method calculateSpeed(...)
-        double result = calculateSpeed(averageSkidLength, dragFactor, breakingEfficiency);
+        // set variable equal to the return from the method No. 3 calculateSpeed(...)
+        double result = calculateSpeed(avgSkidLength, dragFactor, breakEff);
 
-
-        // create html from the method createHTMLDoc(...)
-        StringBuffer stringBuffer = createHTMLDoc(result);
-
+        // create html from the method No. 4 createHTMLDoc(...)
+        StringBuffer stringBuffer = createHTMLDoc(result, skidLen1, skidLen2, skidLen3, skidLen4, surfaceType);
 
         response.setContentType("text/html"); // content type
         PrintWriter printWriter = response.getWriter();
@@ -77,13 +85,48 @@ public class ScreechServlet extends HttpServlet {
     }
 
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+    // calculate the average skid distance
+    public double calculateAverageSkidDistance(int numberOfSkidMarks, double sk1, double sk2, double sk3, double sk4) {
+        averageSkidLength = (sk1 + sk2 + sk3 + sk4) / numberOfSkidMarks;
+        return averageSkidLength;
+    }
+
+
+    // set value based on checkbox selection
+    public double setDragFactor(String choice) {
+        if(choice.equals("Cement")) {
+            dragFactor = 0.75;
+        } else if(choice.equals("Asphalt")) {
+            dragFactor = 0.9;
+        } else if(choice.equals("Gravel")) {
+            dragFactor = 0.8;
+        } else if(choice.equals("Snow")) {
+            dragFactor = 0.25;
+        } else if(choice.equals("Ice")) {
+            dragFactor = 0.55;
+        }
+        return dragFactor;
+    }
+
+
+    // calculate breaking efficiency based on number of skid marks
+    public double calculateBreakingEfficiency(int numberOfSkidMarks) {
+        if(numberOfSkidMarks == 4) {
+            breakingEfficiency = 1.0;
+        } else if(numberOfSkidMarks == 3) {
+            breakingEfficiency = 0.7;
+        } else if(numberOfSkidMarks == 2) {
+            breakingEfficiency = 0.6;
+        } else if(numberOfSkidMarks == 1) {
+            breakingEfficiency = 0.3;
+        }
+        return breakingEfficiency;
     }
 
 
     // calculate the result, using the formula:   S = √30 * D * f * n
-    public double calculateSpeed(double skidDist, double dragFact, double breakingEffy) {
+    public double calculateSpeed(double skidDist, double dragFact, double brakeEfficiency) {
         double product = EQUATION_CONST * skidDist * dragFact;
         double total = Math.sqrt(product);
         double totalRounded = Math.round(total * 10) / 10.0; // to one decimal place
@@ -92,14 +135,20 @@ public class ScreechServlet extends HttpServlet {
 
 
     // create and send HTML to the client
-    public StringBuffer createHTMLDoc(double res) {
+    public StringBuffer createHTMLDoc(double res, double sm1, double sm2, double sm3, double sm4, String sur) {
         StringBuffer stringBuff = new StringBuffer();
         stringBuff.append("<html><head>\n");
         stringBuff.append("<title>Screech GET Result</title>\n");
         stringBuff.append("</head><body>\n");
         stringBuff.append("<jsp:getProperty name='ScreechBean' property='skidMarks'/>");
-        stringBuff.append("<h3>Base on your figures, the cars speed was:</h3>\n");
-        stringBuff.append("<h3>" + res + "mph</h3>");
+        stringBuff.append("<h3>Base on your figures, the " + carName + " details are:</h3>\n");
+        stringBuff.append("<h3>Skid mark 1: " + sm1 + "feet</h3>");
+        stringBuff.append("<h3>Skid mark 2: " + sm2 + "feet</h3>");
+        stringBuff.append("<h3>Skid mark 3: " + sm3 + "feet</h3>");
+        stringBuff.append("<h3>Skid mark 4: " + sm4 + "feet</h3>");
+        stringBuff.append("<h3>Avg skid distance: " + averageSkidLength + "feet</h3>");
+        stringBuff.append("<h3>Surface type: " + sur + "</h3>");
+        stringBuff.append("<h3>Calculated Speed: " + res + "mph</h3>");
         stringBuff.append("</body></html>");
         return stringBuff;
     }
--- web/index.jsp
+++ web/index.jsp
@@ -3,38 +3,66 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
-    <jsp:useBean id="ScreechBean" scope="session" class="main.java.beans.ScreechBean" />
-    <jsp:setProperty name="ScreechBean" property="*" />
+    <jsp:useBean id="ScreechBean" scope="session" class="main.java.beans.ScreechBean"/>
+    <jsp:setProperty name="ScreechBean" property="*"/>
     <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-
     <title>Screech</title>
+
+    <style type="text/css">
+
+        * {
+            margin-bottom: 10px;
+        }
+
+        .submit_btn{
+            margin-right: 30px;
+        }
+
+        .skid_lenghts{
+            display: block;
+            min-width: 150px;
+        }
+
+        input {
+            margin-left: 20px;
+        }
+    </style>
+
 </head>
-<body>
 <form method="get" action="ScreechServlet" method="post">
-    <pre>
+
     <strong class="test">Calculate speed of your car from it's skid marks<br>
     </strong><br>
 
-        Enter skid distance:<br>
-        <input type="text" name="skidlength"><br>
 
-        Enter Number of skid marks:<br>
-        <input type="text" name="skidmark"><br>
+    <label>Enter name of car:</label><br>
+    <input type="text" name="skidlength"><br>
 
-        <label>Enter type of road surface:</label><br>
-        <input type="radio" name="surface" value="Cement">Portland Cement<br>
-        <input type="radio" name="surface" value="Asphalt">Asphalt<br>
-        <input type="radio" name="surface" value="Gravel">Gravel<br>
-        <input type="radio" name="surface" value="Ice">Ice<br>
-        <input type="radio" name="surface" value="Snow">Snow<br>
+    <label>Enter Number of skid marks:</label><br>
+    <input type="text" name="skidmark">
 
-        <input type="range" name="numInput" id="numInputId" value="24" min="1" max="100" oninput="numOutputId.value = numInputId.value">
-        <output name="numOutputName" id="numOutputId">24</output>
+    <div>
+        <label>Skidmark Length No. 1:</label>
+        <input type="number" min="1" max="100" step="0.5" value="0" name="skidmarklength1" class="skid_lenghts">
+        <label>Skidmark Length No. 2:</label>
+        <input type="number" min="0" max="100" step="0.5" value="0" name="skidmarklength2" class="skid_lenghts">
+        <label>Skidmark Length No. 3:</label>
+        <input type="number" min="0" max="100" step="0.5" value="0" name="skidmarklength3" class="skid_lenghts">
+        <label>Skidmark Length No. 4:</label>
+        <input type="number" min="0" max="100" step="0.5" value="0" name="skidmarklength4" class="skid_lenghts">
+    </div>
 
-        <input type="submit" name="submit" value="Submit"><br>
-        <input type="reset" name="reset" value="Reset">
+    <label>Enter type of road surface:</label><br>
+    <input type="radio" name="surface" value="Cement">Portland Cement<br>
+    <input type="radio" name="surface" value="Asphalt">Asphalt<br>
+    <input type="radio" name="surface" value="Gravel">Gravel<br>
+    <input type="radio" name="surface" value="Ice">Ice<br>
+    <input type="radio" name="surface" value="Snow">Snow<br>
 
-    </pre>
+
+    <input class="submit_btn" type="submit" name="submit" value="Submit">
+    <input type="reset" name="reset" value="Reset">
+
 </form>
 
 
